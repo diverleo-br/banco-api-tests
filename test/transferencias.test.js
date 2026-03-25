91d7509d67c09a026f77632cbe555ac9bf805967
Leonardo Euzebio <diverleosp@gmail.com>
2026-03-25 17:18:03 -0300

    altecao nos testes de transferencias

---
+++ test/transferencias.test.js
@@ -0,0 +1,59 @@
+const request = require("supertest")
+const {expect } = require ('chai')
+
+describe ('Transferências', () =>{
+    describe('POST /transferencias', () => {
+        it('Deve retornar sucesso 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
+           //
+            const respostaLogin = await request ('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'leo',
+                    'senha': '1234'
+                })
+            const token = respostaLogin.body.token
+
+            const resposta = await request ('http://localhost:3000')
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', 'Bearer ' + token)
+            .send({
+                contaOrigem: 2,
+                contaDestino: 3,
+                valor: 11,
+                token: ""
+                })
+            expect(resposta.status).to.equal(201)
+
+            console.log(resposta.body)
+        })
+
+        it ('Deve retornar falha 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
+             const respostaLogin = await request ('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'leo',
+                    'senha': '1234'
+                })
+            const token = respostaLogin.body.token
+
+            const resposta = await request ('http://localhost:3000')
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', 'Bearer ' + token)
+            .send({
+                contaOrigem: 2,
+                contaDestino: 3,
+                valor: 7,
+                token: ""
+                })
+            expect(resposta.status).to.equal(422)
+
+            console.log(resposta.body)
+
+    })
+
+    })
+} )
