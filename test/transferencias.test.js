bad438320b87f94432de6f2a850f5144ff9c1eb0
Leonardo Euzebio <diverleosp@gmail.com>
2026-03-25 22:55:09 -0300

    melhorando o cod com o helpers - criado helper de autenticacao

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,20 +1,23 @@
 const request = require("supertest")
 const {expect } = require ('chai')
 require('dotenv').config()
+const { obterToken } = require('../helpers/autenticacao.js')
 
 
 describe ('Transferências', () =>{
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-           //
-            const respostaLogin = await request (process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'leo',
-                    'senha': '1234'
-                })
-            const token = respostaLogin.body.token
+            //apos mudanca no cod usamos o comando para chamar o helpers (funcao passando user e senha)
+            const token = await obterToken('leo','1234')
+        //cod antes da melhoria de helpers
+        //     const respostaLogin = await request (process.env.BASE_URL)
+        //         .post('/login')
+        //         .set('Content-Type', 'application/json')
+        //         .send({
+        //             'username': 'leo',
+        //             'senha': '1234'
+        //         })
+        //     const token = respostaLogin.body.token
 
             const resposta = await request (process.env.BASE_URL)
             .post('/transferencias')
@@ -32,14 +35,15 @@ describe ('Transferências', () =>{
         })
 
         it ('Deve retornar falha 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
-             const respostaLogin = await request ('http://localhost:3000')
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'leo',
-                    'senha': '1234'
-                })
-            const token = respostaLogin.body.token
+            const token = await obterToken('leo','1234')
+            //  const respostaLogin = await request ('http://localhost:3000')
+            //     .post('/login')
+            //     .set('Content-Type', 'application/json')
+            //     .send({
+            //         'username': 'leo',
+            //         'senha': '1234'
+            //     })
+            // const token = respostaLogin.body.token
 
             const resposta = await request ('http://localhost:3000')
             .post('/transferencias')
