2409705a7ff0b3017db8d12dccad068ae15bf2a5
Leonardo Euzebio <diverleosp@gmail.com>
2026-03-27 13:26:14 -0300

    colocando mais melhorias utilizando fixtures

--- test/login.test.js
+++ test/login.test.js
@@ -1,17 +1,16 @@
 const request = require("supertest")
 const {expect } = require ('chai')
 require('dotenv').config()
+const postLogin = require ('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe ('POST/LOGIN', () => {
         it('Deve retornar 200 com tokem em string quando usar credenciais validas', async ()=> {
+            const bodyLogin = {...postLogin}
             const resposta = await request (process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'leo',
-                    'senha': '1234'
-                })
+                .send(bodyLogin)
             //mostra somente o conteudo e o status da variavel resposta
             console.log(resposta.status);
             console.log(resposta.body)
